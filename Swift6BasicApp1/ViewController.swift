11aa149750219bfae0ade0851f5bf929955317bd
yuri2509 <gatx207-nicol@docomo.ne.jp>
2021-03-21 22:14:46 +0900

    Initial Commit

---
+++ Swift6BasicApp1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift6BasicApp1
+//
+//  Created by ゆり on 2021/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
