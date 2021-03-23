73549778e7edc29db12b04c7a074f61efdf409ea
yuri2509 <gatx207-nicol@docomo.ne.jp>
2021-03-24 00:13:04 +0900

    変数などの追加

--- Swift6BasicApp1/ViewController.swift
+++ Swift6BasicApp1/ViewController.swift
@@ -8,7 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    @IBOutlet weak var blurView: UIVisualEffectView!
+    
+    @IBOutlet weak var countLabel: UILabel!
+    
+    var count= 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
