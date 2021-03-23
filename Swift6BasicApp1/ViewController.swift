01e2b473c86dbdfa69939e349a0711267c2341b6
yuri2509 <gatx207-nicol@docomo.ne.jp>
2021-03-24 00:25:19 +0900

    buttonを押した時の動作

--- Swift6BasicApp1/ViewController.swift
+++ Swift6BasicApp1/ViewController.swift
@@ -11,18 +11,24 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
     
-    
     @IBOutlet weak var blurView: UIVisualEffectView!
     
     @IBOutlet weak var countLabel: UILabel!
     
-    var count= 0
+    var count = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func tap(_ sender: Any) {
+        
+        count = count + 1
+        countLabel.text = count
+        
+    }
+    
 }
 
